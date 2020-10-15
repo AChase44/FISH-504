78a70f5a1161b79489e04c4eae2cbf782ec0875f
IceDirt <acrobaticchase@gmail.com>
2020-10-14 23:47:20 -0700

    Commit test 2

---
+++ src/Fix_for_username_email_error_during_commit.R
@@ -0,0 +1,8 @@
+#Go to tools, click Shell... to open the command window. 
+
+#type
+git config --global user.email "acrobaticchase@gmail.com" #hit enter
+#type
+git config --global user.name "IceDirt" #hit enter
+
+#this should resolve the issue. 
