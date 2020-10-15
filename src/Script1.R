51632487ee1a6b4a87a82b5309a4851ae2b62854
IceDirt <acrobaticchase@gmail.com>
2020-10-14 23:45:07 -0700

    commit practice 2

--- src/Script1.R
+++ src/Script1.R
@@ -1,2 +1,5 @@
 git remote add origin https://github.com/IceDirt/Testing.git
 https://github.com/IceDirt/Testing.git
+
+git config --global user.email "acrobaticchase@gmail.com"
+git config --global user.name "IceDirt"
